3bf7b12e92d2c3a1170101a4c29bc25e1daee027
Nikita dange <nikitadange227@gmail.com>
2026-03-17 19:14:19 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+echo "Deployment started"
+
+# Apache install (if not installed)
+sudo yum install -y httpd
+
+# Start Apache server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Copy files
 sudo cp -r * /var/www/html/
+
+echo "Deployment completed"
