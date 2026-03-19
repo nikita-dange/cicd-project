a51dc1fc4783e57efe7865ae77fe98c50725de23
Nikita dange <nikitadange227@gmail.com>
2026-03-19 13:56:13 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-
-
 sudo apt update -y
 sudo apt install apache2 -y
-
-ा
 sudo systemctl start apache2
-sudo systemctl enable apache2
-
-ा
 sudo rm -rf /var/www/html/*
-
-
 sudo cp -r /home/ubuntu/my-app/* /var/www/html/
-
-ा
-sudo chown -R www-data:www-data /var/www/html/
