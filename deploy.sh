0cddaad184836d14a42dd3d7c43766e3755c3143
Nikita dange <nikitadange227@gmail.com>
2026-03-19 13:37:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-echo "Deployment started"
 
-cd /opt/codedeploy-agent/deployment-root/*/*/deployment-archive
-
-# Install Apache (Ubuntu)
 sudo apt update -y
 sudo apt install apache2 -y
 
-# Start Apache
+ा
 sudo systemctl start apache2
 sudo systemctl enable apache2
 
-# Clean old files
+ा
 sudo rm -rf /var/www/html/*
 
-# Copy new files
-sudo cp -r * /var/www/html/
 
-echo "Deployment completed"
+sudo cp -r /home/ubuntu/my-app/* /var/www/html/
+
+ा
+sudo chown -R www-data:www-data /var/www/html/
